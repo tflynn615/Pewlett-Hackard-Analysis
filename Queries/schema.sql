41f475e0430957a678b01ca43bc6c065b3eb9448
tflynn615 <78891722+tflynn615@users.noreply.github.com>
2021-04-18 22:52:59 -0500

    Adding schemas and retirement Info query

---
+++ Queries/schema.sql
@@ -0,0 +1,54 @@
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+				dept_no varchar(4) not null,
+				dept_name varchar(40) not null,
+				PRIMARY KEY (dept_no),
+				UNIQUE (dept_name));
+				
+-- Create employees table 
+create table employees ( emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
+);
+
+-- New Table 
+create table dept_manager(
+		dept_no VARCHAR(4) NOT NULL, 
+		emp_no INT NOT NULL,
+		from_date DATE NOT NULL, 
+		to_date DATE NOT NULL,
+		FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
+		FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+		PRIMARY KEY (emp_no, dept_no));
+		
+-- New Table 
+CREATE TABLE salaries(
+	emp_no INT NOT NULL, 
+	salary INT NOT NULL, 
+	from_date DATE NOT NULL, 
+	to_date DATE NOT NULL, 
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no));
+	
+create table dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no));
+	
+create table titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no));
+	
+SELECT * from departments;
